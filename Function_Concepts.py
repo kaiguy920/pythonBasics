85bb84a7a4a8403a0c03b235aa2677fa77d580f3
kaiguy920 <smithkai920@gmail.com>
2022-10-31 13:49:31 -0400

    adds function & decorator concepts

--- Function_Concepts.py
+++ Function_Concepts.py
@@ -80,3 +80,18 @@ empData(address="India", number=9876543210, name="KumaR")
 
 def methodThree():
     pass
+
+
+"""
+A lambda function is a small anonymous function.
+A lambda function can take any number of arguments, but can only have one expression.
+
+Syntax: lambda arguments : expression
+
+"""
+
+x= lambda a : a+10
+print(x(5))
+
+x= lambda a,b : a+b+10
+print(x(5,10))
